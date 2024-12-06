8fd90ec1b780b34e12f36e925a2ea2354cc36e92
aicafiles <aicarandang088@gmail.com>
2024-12-06 13:04:57 +0800

    Add media query for mobile responsiveness

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'screens/auth/log_in.dart';
 import 'screens/main_screens/home.dart';
 import 'screens/main_screens/favorites.dart';
@@ -24,24 +25,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'NutriWise',
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
-            final user = snapshot.data;
-            if (user == null) {
-              return const LoginScreen();
-            } else {
-              return const MainNavigation();
-            }
-          }
-          return const CircularProgressIndicator();
-        },
-      ),
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      minTextAdapt: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'NutriWise',
+          theme: ThemeData(primarySwatch: Colors.green),
+          home: StreamBuilder<User?>(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                final user = snapshot.data;
+                if (user == null) {
+                  return const LoginScreen();
+                } else {
+                  return const MainNavigation();
+                }
+              }
+              return const CircularProgressIndicator();
+            },
+          ),
+        );
+      },
     );
   }
 }
@@ -89,21 +96,21 @@ class _MainNavigationState extends State<MainNavigation> {
     return AnimatedContainer(
       duration: const Duration(milliseconds: 300),
       curve: Curves.easeInOut,
-      height: isSelected ? 46 : 40,
-      width: isSelected ? 46 : 40,
+      height: isSelected ? 46.sp : 40.sp,
+      width: isSelected ? 46.sp : 40.sp,
       decoration: BoxDecoration(
         color: isSelected ? Colors.white : Colors.transparent,
         borderRadius: BorderRadius.circular(12),
         border: Border.all(
           color: isSelected ? Colors.green : Colors.transparent,
-          width: 2,
+          width: 2.sp,
         ),
         boxShadow: isSelected
             ? [
           BoxShadow(
             color: Colors.green.withOpacity(0.4),
-            blurRadius: 6,
-            spreadRadius: 1,
+            blurRadius: 6.sp,
+            spreadRadius: 1.sp,
           ),
         ]
             : [],
@@ -112,7 +119,7 @@ class _MainNavigationState extends State<MainNavigation> {
         child: FaIcon(
           icon,
           color: isSelected ? Colors.green : Colors.green.shade500,
-          size: isSelected ? 22 : 20,
+          size: isSelected ? 22.sp : 20.sp,
         ),
       ),
     );
@@ -176,20 +183,20 @@ class _MainNavigationState extends State<MainNavigation> {
       bottomNavigationBar: Container(
         color: Colors.white,
         child: Container(
-          height: 50,
-          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          height: 50.h,
+          margin: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(30),
             border: Border.all(
               color: Colors.green.shade700,
-              width: 3,
+              width: 3.w,
             ),
             boxShadow: [
               BoxShadow(
                 color: Colors.black.withOpacity(0.1),
-                blurRadius: 8,
-                spreadRadius: 2,
+                blurRadius: 8.r,
+                spreadRadius: 2.r,
               ),
             ],
           ),
